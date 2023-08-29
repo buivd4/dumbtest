a981e77150bad76d43d42400a22da227184ae97c
Duong Bui Van <vanduong.bui@vn.panasonic.com>
2023-08-29 10:01:41 +0700

    Bug fix

--- lazytest/script_loader.py
+++ lazytest/script_loader.py
@@ -8,5 +8,5 @@ def load_iterators(dir):
     modules = glob.glob(os.path.join(dir, "*.py"))
     cls=[]
     for module in modules:
-        cls.extend([c for n,c in inspect.getmembers(importlib.import_module(module.replace("/",".").removesuffix(".py")), inspect.isclass)])
+        cls.extend([c for n,c in inspect.getmembers(importlib.import_module(module.replace("/",".").replace("\\",".").removesuffix(".py")), inspect.isclass)])
     return [c for c in cls if c is not AbstractTestIterator and issubclass(c, AbstractTestIterator)]
